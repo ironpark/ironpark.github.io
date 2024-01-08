9d95125e8353312e270f03f1de3e8ede6eb63876
ironpark <cjfdhksaos@gmail.com>
2024-01-09 01:02:19 +0900

    blog basecode update

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,26 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+    import type { PageData } from './$types'
+
+    export let data: PageData
+    let posts = data.posts
+    let publishedPosts = posts.filter(p => !p.draft)
+</script>
+
+<svelte:head>
+    <title>웹사이트 제목</title>
+    <meta name="description" content="웹사이트 설명" />
+</svelte:head>
+
+<h1>웹사이트 제목</h1>
+
+<ul class="posts">
+    {#each posts as post}
+        <li>
+            <article>
+                <h2 class="title"><a href={`/posts/${post.id}`}>{post.title}</a></h2>
+                <time class="published-at">{post.publishedAt}</time>
+                <p class="summary">{post.summary}</p>
+            </article>
+        </li>
+    {/each}
+</ul>
---
+++ src/routes/posts/[slug]/+page.svelte
@@ -0,0 +1,13 @@
+<!-- src/routes/[slug]/+page.svelte -->
+
+<script>
+    import { Utterances } from "@codewithshin/svelte-utterances";
+    export let data;
+</script>
+
+<article>
+    <h1>{data.title}</h1>
+    <p>Published: {data.date}</p>
+    <svelte:component this={data.content} />
+    <Utterances reponame="ironpark/ironpark.github.io" theme="github-dark" />
+</article>
