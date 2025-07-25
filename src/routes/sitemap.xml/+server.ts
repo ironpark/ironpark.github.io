9e73b6111e7f299d43ba67712da833a7c8e80b94
ironpark <cjfdhksaos@gmail.com>
2025-07-26 06:42:42 +0900

    sitemap namespace fix

--- src/routes/sitemap.xml/+server.ts
+++ src/routes/sitemap.xml/+server.ts
@@ -92,7 +92,7 @@ export const GET: RequestHandler = async () => {
 	
 	// Generate sitemap XML
 	let xml = `<?xml version="1.0" encoding="UTF-8"?>
-<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">`;
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:xhtml="http://www.w3.org/1999/xhtml">`;
 	
 	// Add static pages
 	staticPages.forEach(page => {
