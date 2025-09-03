517fb14f5f3f980de2bdd5246209094a70a1a020
ironpark <cjfdhksaos@gmail.com>
2025-09-03 22:24:06 +0900

    fix: temp dir

--- main.ts
+++ main.ts
@@ -8,7 +8,7 @@ import utils from './lib/utils.js'
 import { Cache } from './lib/cache.js'
 
 // create temp dir
-const tempDir = "posts"
+const tempDir = "temp"
 
 const config = {
     originalPosts: "posts",
